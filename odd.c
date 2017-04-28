0f0a87a160452269b6288e3e47b50dec8d30783b
vinitha-vini <vinithav941@gmail.com>
2017-04-28 09:04:14 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,14 @@
+# vini2
+#include<stdio.h>
+void main()
+{
+int num;
+int n=20,m=30;
+printf("Odd numbers between the limits are:");
+for(num=n;num<=m;num++)
+{
+if(num%2==1)
+{
+printf("%d",num);
+}
+}}
