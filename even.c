836e76b9e9e72513677ee02e3e4c30aeacfd606c
vinitha-vini <vinithav941@gmail.com>
2017-04-28 09:13:56 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,15 @@
+# vini2
+#include<stdio.h>
+void main()
+{
+int i=1,n;
+printf("enter a limit:");
+scanf("%d",&n);
+printf("even numbers from between 1 and %d are:",n);
+while(i<=n)
+{
+if(i%2==0)
+{
+printf("%d",i);
+}
+}}
